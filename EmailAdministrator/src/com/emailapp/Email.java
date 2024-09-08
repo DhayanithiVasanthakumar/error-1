23433376a587f1cb62361f2c1b208a0265d5b14d
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-09-08 19:38:47 +0530

    Random password

--- EmailAdministrator/src/com/emailapp/Email.java
+++ EmailAdministrator/src/com/emailapp/Email.java
@@ -6,6 +6,7 @@ public class Email {
 	private String firstname;
 	private String lastname;
 	private String password;
+	private int defaultPasswordLength=10;
 	private String alternateemail;
 	private String department;
 	private int mailcapacity;
@@ -22,7 +23,13 @@ public class Email {
 		//call a method asking for the department return the department
 		this.department=setDepartment();
 		System.out.println("Department :"+" "+this.department);
+		
+		//call a method to generate a random password
+		this.password=randomPassword(defaultPasswordLength);
+		System.out.println("Your password is :"+this.password);
 	}
+	
+	
 	
 	//ask for a department from user
 	private String setDepartment(){
@@ -44,7 +51,20 @@ public class Email {
 			return "None";
 	}
 	
+	
+	
 	//Generate a random password.
+	private String randomPassword(int length) {
+		String passwordSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$";
+		char[] password =new char[length];
+		for(int i=0;i<length;i++) {
+			int rand=(int)(Math.random()*passwordSet.length());
+			password[i]=passwordSet.charAt(rand);
+		}
+		return new String(password);
+	}
+	
+	
 	
 	//mail capacity
 	
