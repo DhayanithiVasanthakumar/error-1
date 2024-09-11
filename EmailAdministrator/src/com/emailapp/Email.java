0392b739a43db2b7b2c1e36bf16cab0021583826
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-09-11 12:30:33 +0530

    add new features

--- EmailAdministrator/src/com/emailapp/Email.java
+++ EmailAdministrator/src/com/emailapp/Email.java
@@ -1,10 +1,13 @@
 package com.emailapp;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Email  {
+	public Scanner in=new Scanner(System.in);//Scanner class(global)for this class -> it access it in any where in this class
 	/*
-	*declare variables
+	*declare global variables-> as private for Security issues.
+	*it does not access directly.
 	*/
 	private String firstname;
 	private String lastname;
@@ -29,18 +32,26 @@ public class Email  {
 		
 		
 		//call a method asking for the department return the department
-		this.department=setDepartment();
+		//this.department=setDepartment();
 		//System.out.println("Department :"+" "+this.department);
 		
 		
 		//call a method to generate a random password
-		this.password=randomPassword(defaultPasswordLength);
-		System.out.println("\nYour password is :"+this.password);
+		//this.password=randomPassword(defaultPasswordLength);
+		//System.out.println("\nYour password is :"+this.password);
 		
 		
 		//combine elements to generate a email.
-		email=firstname.toLowerCase()+"."+lastname.toLowerCase()+"@"+department+"."+companySuffix;
+		//email=firstname.toLowerCase()+"."+lastname.toLowerCase()+"@"+department+"."+companySuffix;
 		//System.out.println("your email is :"+email);
+		
+		/*
+		 * calling methods
+		 */
+		this.department=this.setDepartment();//department
+		this.password=this.randomPassword(8);//password
+		this.email=this.mailMethod();//email generation
+		
 		
 	}
 	
@@ -50,24 +61,70 @@ public class Email  {
 	/*
 	 * methods
 	 */
-	//ask for a department from user
+	
+	//generate mail method.
+	private String mailMethod() {
+		return this.firstname.toLowerCase()+"."+this.lastname.toLowerCase()+"@"+this.department.toLowerCase()+"."+this.companySuffix;
+	}
+	
+	//ask for a department Method from user
 	private String setDepartment(){
-		System.out.print("New Worker :"+firstname+"\n\nDepartment codes\n1: Sales\n2: Development\n3: Accounting\n0: none\nEnter the Department code:");
-		//to get department number form user
-		Scanner in=new Scanner(System.in);
-		int departmentChoice=in.nextInt();
+		System.out.print("New Worker :"+firstname+"\n\nDepartment codes\n1: Sales\n2: Development\n3: Accounting\n0: none ");
 		
-		if(departmentChoice==1) { return "sales"; }
-		else if(departmentChoice==2) { return "Development"; }
-		else if(departmentChoice==3) { return "Accounting"; }
-		else 
-			return "None";
+		boolean flag=false;
+		do {
+			System.out.println("Enter Department code :");
+			//Scanner class is defined in global.
+			//to get department number form user
+			int departmentChoice=in.nextInt();
+			
+			//use switch it is faster
+			switch(departmentChoice) {
+			case 1:
+				return "sales";
+			case 2:
+				return "Development";
+			case 3:
+				return "Accounting";
+			case 0:
+				return "None";
+			default:
+				return "Invalid Choice,Please choose it again:";
+			}
+			
+			/* if else is slower compare to switch 
+			if(departmentChoice==1) { return "sales"; }
+			else if(departmentChoice==2) { return "Development"; }
+			else if(departmentChoice==3) { return "Accounting"; }
+			else 
+				return "None";
+			*/	
+		}
+		//!flag-> it make true,so again run-> do condition.
+		while(!flag);
 	}
 	
 	
 	
-	//Generate a random password.
+	
+	//Generate a random password method.
 	private String randomPassword(int length) {
+		//Random is one of the class in java.it choose a give string randomly.
+		Random r=new Random();
+		String capitalLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String smallLetters="abcdefghijklmnopqrstuvwxyz";
+		String numbers="1234567890";
+		String symbols="!@#$%";
+		
+		String values=capitalLetters+smallLetters+numbers+symbols;
+		String password="";
+		for(int i=0;i<length;i++) {
+			password=password+values.charAt(r.nextInt(values.length()));
+		}
+		return password;
+		
+		/* 
+		 * another way to create an random password.
 		String passwordSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$";
 		char[] password =new char[length];
 		for(int i=0;i<length;i++) {
@@ -75,29 +132,85 @@ public class Email  {
 			password[i]=passwordSet.charAt(rand);
 		}
 		return new String(password);
+		*/
 	}
+	
+	
+	//change password method -> it is user activity so it is in public. 
+	//user will access it.
+	public void setPassword() {
+		boolean flag=false;
+		do {
+			System.out.println("Do you want to change password: (Y/N)");
+			//index start with 0th position.
+			//so charAt() type cast string it read at 0th index
+			char passwordChoice=in.next().charAt(0);
+			
+			if(passwordChoice=='Y'||passwordChoice=='y') {
+				//flag is true -> it stop do while loop and run below statements 
+				flag=true;
+				System.out.println("Enter current password: ");
+				String currentPassword=in.next();
+				//current password equals old password
+				if(currentPassword.equals(this.password)) {
+					System.out.println("Enter new password: ");
+					this.password=in.next();
+					System.out.println("Password changed sucessfully.");
+				}
+				else {
+					System.out.println("Incorrect password/");
+				}
+			}
+			else if(passwordChoice=='N'||passwordChoice=='n') {
+				flag=true;
+				System.out.println("Password change option cancelled!");
+			}
+			else {
+				System.out.println("Enter valid choice: ");
+			}
+		}while(!flag);
+	}
+	
+
+	//set mailBoxCapacity method -> it is user activity so it is in public.
+		//user will access it.
+		public void setMailBoxCapacity() {
+			System.out.println("Current mail box capacity :"+this.mailcapacity+"mb");
+			System.out.println("Enter new mail boc capacity :");
+			this.mailcapacity=in.nextInt();
+			System.out.println("Mail box capacity sucessfully updated");
+		}
+		
+	//set alternate mail method
+		public void setAlternateEmail() {
+			System.out.println("Enter alternate mail: ");
+			this.email=in.next();
+			System.out.println("Altername email is updated");
+		}
+	
+	
 	
 	
 	
 	
 	/*
-	 * set methods
+	 * set methods->these all are user activity so it is in public.
 	 */
 	//set the mail capacity
-	public void setMailBoxCapacity(int capacity) {
-		this.mailcapacity=capacity;
-	}
+//	public void setMailBoxCapacity(int capacity) {
+//		this.mailcapacity=capacity;
+//	}
 	
 	
 	//set the alternative email
-	public void setAlternativeEmail(String alternateEmail) {
-		this.alteremail=alternateEmail;
-	}
+//	public void setAlternativeEmail(String alternateEmail) {
+//		this.alteremail=alternateEmail;
+//	}
 	
 	//change password
-	public void setPassword(String changedpassword) {
-		this.password=changedpassword;
-	}
+	//public void setPassword(String changedpassword) {
+	//	this.password=changedpassword;
+	//}
 	
 	
 	/*
--- EmailAdministrator/src/com/emailapp/EmailApp.java
+++ EmailAdministrator/src/com/emailapp/EmailApp.java
@@ -1,9 +1,46 @@
 package com.emailapp;
 
+import java.util.Scanner;
+
 public class EmailApp {
 	public static void main(String[]args) {
-		Email em=new Email("dhaya", "aswini");
 		
-		System.out.println(em.showInfo());
+		Scanner in=new Scanner(System.in);
+		//user information
+		System.out.println("Enter firstname: ");
+		String fname=in.next();
+		System.out.println("Enter lastname: ");
+		String lname=in.next();
+		
+		
+		//create obj for Email class
+		Email em=new Email(fname,lname);
+		//  why -1 ->because we use (flag) true/false  true represent 1,false represent 0 in Email class
+		//invoke it again and again
+		int choice=-1;
+		do {
+			System.out.println("\n *******\nEnter Choice: \n1 show information\n2 change password\n3 change mailbox capacity\n4 set alternative mail\n5 Exit");
+			choice=in.nextInt();
+			switch(choice) {
+			case 1:
+				em.showInfo();
+				break;
+			case 2:
+				em.setPassword();
+				break;
+			case 3:
+				em.setMailBoxCapacity();
+				break;
+			case 4:
+				em.setAlternateEmail();
+				break;
+			case 5:
+				System.out.println("Thankyou for using Application");
+				break;
+			default:
+				System.out.println("Invalid choice,please enter valid choice");
+			}
+		}while(choice!=5);
+
 	}
 }
